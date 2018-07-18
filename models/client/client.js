e2c2eb07f7f0fc3b625c62eebaa5b20429326ae1
Dengfeng Liu <liudengfeng@kunteng.org>
2018-07-18 17:29:47 +0800

    Update client.js

--- models/client/client.js
+++ models/client/client.js
@@ -5,27 +5,27 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema;
 
 const clientSchema = new Schema({
-	gwId: 		String,
+	gwId: 		String, // 设备ID
 	clients:[
 		{
-			mac:		String,
-			ip:			String,
-			token:		String,
-			wired:		Number,
-			name:		String,
-			incoming:	Number,
-			outcoming:	Number,
-			firstLogin:	Number,
-			onlineTime:	Number,
-			incomingdelta:	Number,
+			mac:		String, // 终端mac
+			ip:			String, // 终端ip
+			token:		String, // 终端授权token
+			wired:		Number, // 是否有线设备
+			name:		String, // 终端名称
+			incoming:	Number, // 下行流量
+			outcoming:	Number, // 上行流量
+			firstLogin:	Number, // 通过认证时间
+			onlineTime:	Number, // 在线时长
+			incomingdelta:	Number, 
 			outcomingdelta:	Number,
-			channelPath:	String,
-			lastTime:		Number
+			channelPath:	String, 
+			lastTime:		Number // 最近访问时间
 		}
 	]
 })
 
-clientSchema.index({gwId: 1});
+clientSchema.index({mac: 1});
 
 const Client = mongoose.model('Client', clientSchema);
 
