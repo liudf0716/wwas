40337c8128d85dc5ed5c00df6d5ed590e83cf53e
Dengfeng Liu <liudengfeng@kunteng.org>
2018-07-19 15:04:34 +0800

    Update setting.js

--- routes/setting.js
+++ routes/setting.js
@@ -4,6 +4,7 @@ import express from 'express'
 import Setting from '../controller/setting/setting'
 const router = express.Router();
 
-router.post('/setConfig', Setting.setConfig);
+router.post('/channelpathSetting', Setting.channelpathSetting);
+router.post('/gwidSetting', Setting.gwidSetting);
 
 export default router
