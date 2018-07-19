35022fc45aaae7089a9666250bfe082d77a010a3
Dengfeng Liu <liudengfeng@kunteng.org>
2018-07-19 14:45:47 +0800

    Create gatewayid.js

---
+++ models/setting/gatewayid.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema;
+
+const gatewayIdSchema = new Schema({
+	gwId: 			String,
+	weixin: {
+		appId: {type: String, default: 'wxfb684aa755dffceb'},
+		shopId: {type: Number, default: '641418'},
+		secretKey: {type: String, default: 'ca0ddbac646160edfeaf343937f73404'},
+		ssid: {type: String, default: 'ApFreeWiFiDog'}
+	},
+	wificoin: {
+		toAddress: {type: String, default: 'wZirordpuoJgmRp6wRPKZjAjVruQr5gF7r'},
+		toAmount: {type: Number, default: 2},
+	},
+	portalUrl: {type: String, default: 'https://talkblock.org/'},
+	duration: {type: Number, default: 3600}
+})
+
+gatewayIdSchema.index({gwId: 1});
+
+const GatewayId = mongoose.model('GatewayId', gatewayIdSchema);
+
+
+export default GatewayId
