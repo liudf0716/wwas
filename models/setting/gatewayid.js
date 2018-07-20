eca3b03b5f217dc576cdcb606917b28ea716d5f8
Dengfeng Liu <liudf0716@gmail.com>
2018-07-20 15:21:35 +0800

    Update gatewayid.js

--- models/setting/gatewayid.js
+++ models/setting/gatewayid.js
@@ -6,18 +6,7 @@ const Schema = mongoose.Schema;
 
 const gatewayIdSchema = new Schema({
 	gwId: 			String,
-	weixin: {
-		appId: {type: String, default: 'wxfb684aa755dffceb'},
-		shopId: {type: Number, default: '641418'},
-		secretKey: {type: String, default: 'ca0ddbac646160edfeaf343937f73404'},
-		ssid: {type: String, default: 'ApFreeWiFiDog'}
-	},
-	wificoin: {
-		toAddress: {type: String, default: 'wZirordpuoJgmRp6wRPKZjAjVruQr5gF7r'},
-		toAmount: {type: Number, default: 2},
-	},
-	portalUrl: {type: String, default: 'https://talkblock.org/'},
-	duration: {type: Number, default: 3600}
+    channelPath:{type: String, default: 'wificoin'}
 })
 
 gatewayIdSchema.index({gwId: 1});
