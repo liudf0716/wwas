78e49376ec23157b8e5775ca8b35e075a867b1f0
Dengfeng Liu <liudf0716@gmail.com>
2018-07-20 17:14:25 +0800

    Update wifidog.js

--- controller/wifidog/wifidog.js
+++ controller/wifidog/wifidog.js
@@ -23,7 +23,6 @@ class Wifidog {
 		this.generateTxidRequest	= this.generateTxidRequest.bind(this);
 		this.generateAuthTokenUrl	= this.generateAuthTokenUrl.bind(this);
 		this.getDeviceSetting		= this.getDeviceSetting.bind(this);
-		this.updateDeviceClientFromQuery	= this.updateDeviceClientFromQuery.bind(this);
 		this.login 		= this.login.bind(this);
 		this.ping		= this.ping.bind(this);
 		this.authWfc	= this.authWfc.bind(this);
@@ -31,55 +30,7 @@ class Wifidog {
 	}
 
 	async ping(req, res, next){
-		try {
-		var gwId		= req.query.gw_id;
-		var	sysUptime	= req.query.sys_uptime;
-		var sysMemfree	= req.query.sys_memfree;
-		var sysLoad		= req.query.sys_load;
-		var	cpuUsage	= req.query.cpu_usage; 
-		var	ssid		= req.query.ssid;
-		var	version		= req.query.version;
-		var	type		= req.query.type; 	// router type
-		var	name		= req.query.name;	// router name
-		var	channelPath			= req.query.channel_path;
-		var	wiredPassed			= req.query.wired_passed;
-		var	wifidogUptime		= req.query.wifidog_uptime;
-		var	onlineClients		= req.query.online_clients;
-		var	offlineClients		= req.query.offline_clients;
-		var	nfConntrackCount	= req.query.nf_conntrack_count;
-		var lastTime			= Math.round(+new Date()/1000);
-		var	remoteAddress		= req.connection.remoteAddress;
-		var deviceStatus		= 1;
-		const newDevice = {
-			gwId,
-			sysUptime,
-			sysMemfree,
-			sysLoad,
-			cpuUsage,
-			ssid,
-			version,
-			type,
-			name,
-			channelPath,
-			wiredPassed,
-			wifidogUptime,
-			onlineClients,
-			offlineClients,
-			nfConntrackCount,
-			lastTime,
-			remoteAddress,
-			deviceStatus
-		}
-
-		const device = await DeviceModel.findOne({gwId: gwId});
-		if(!device){
-			await DeviceModel.create(newDevice);
-		} else {
-			await DeviceModel.findOneAndUpdate({gwId}, {$set: newDevice});
-		}
-		}catch(e){
-			console.log(e);
-		}
+		client.updateDeviceFromPing(req);
 		res.send('Pong');
 	}
 
@@ -94,7 +45,9 @@ class Wifidog {
 		var orderNumber = new UniqueNumber(true).generate();
 		var	randomValue = Math.floor(Math.random() * (9999 - 1000 - 1)) + 1000;
 		var orderTime 	= Math.round(+new Date()/1000);
-		const deviceSetting 	= this.getDeviceSetting(gwId);
+		const deviceSetting 	= device.deviceSetting(gwId);
+        if(deviceSetting == null)
+            deviceSetting = device.deviceSetting(gwId);
 		var toAmount	= deviceSetting.wificoin.toAmount + randomValue/1000000;
 		const newOrder = {
 			orderNumber,
@@ -121,11 +74,11 @@ class Wifidog {
 			 wfcAuth: wfcAuthUrl,
 			 gwAddress: gwAddress,
 			 gwPort: gwPort,
-			 appId: deviceSetting.wxAppId,
+			 appId: deviceSetting.weixin.wxAppId,
 			 extend: orderNumber,
 			 timestamp: timestamp,
 			 sign: wxSign,
-			 shopId: deviceSetting.wxShopId,
+			 shopId: deviceSetting.weixin.wxShopId,
 			 authUrl: wxAuthUrl,
 			 mac: staMac,
 			 ssid: ssid,
@@ -143,11 +96,11 @@ class Wifidog {
 				res.send('Auth: 1');
 			}
 		} else if (stage == 'counters') {
-			var result = this.updateDeviceClientFromQuery(req.query);
+			var result = client.updateDeviceClientFromCounter(req.query);
 			res.send('Auth: ' + result);
 		} else if (stage == 'logout') {
 		} else {
-			res.send("unkown stage");
+			res.send("illegal stage");
 		}
 	}
 	async authWeixin(req, res, next){
@@ -235,91 +188,11 @@ class Wifidog {
 	async portal(req, res, next){
 		var gwId 		= req.query.gw_id;
 		var channelPath	= req.query.channel_path;
-		const setting = await SettingModel.findOne({'gwId':gwId});
-		if(setting)
+		const setting = await device.deviceSetting(gwId);
+		if(setting != null)
 			res.redirect(setting.portalUrl);
 		else
 			res.redirect("https://talkblock.org/");
-	}
-	
-	async getDeviceSetting(gwId) {
-		try{
-			const deviceSetting = await SettingModel.findOne({'gwId':gwId});
-			if(deviceSetting)
-				return deviceSetting;
-			
-			const newSetting = {'gwId': gwId };	
-			await SettingModel.create(newSetting);
-		}catch(err){
-			console.log(err);
-		}
-	}
-	
-	async updateDeviceClientFromQuery(query) {
-		try {
-			var mac 	= query.mac;
-			var ip		= query.ip;
-			var token	= query.token;
-			var wired	= query.wired;
-			var	name	= query.name;
-			var	gwId	= query.gw_id;
-			var incoming	= query.incoming;
-			var outcoming	= query.outcoming;
-			var firstLogin	= query.first_login;
-			var onlineTime	= queyr.online_time;
-			var lastTime	= Math.round(+new Date()/1000);
-			var incomingdelta	= query.incomingdelta;
-			var outcomingdelta	= query.outcomingdelta;
-			var	channelPath		= query.channel_path;
-
-			const device = await ClientModel.findOne({'gwId':gwId});
-			if(!device){
-				console.log('impossible: cannot find device: ' + gwId);
-				return 0;
-			}
-			const setting = await SettingModel.findOne({'gwId':gwId});
-			if(!setting){
-				console.log('impossible: cannot find setting of ' + gwId);
-				return 0;
-			}
-			if(setting.duration < (lastTime - firstLogin)){
-				console.log('client timeout ' + mac);
-				return 0;
-			}
-
-			var clients = device.clients;
-			const item = {	
-				mac: 	mac,
-				ip:		ip,
-				token:	token,
-				wired:	wired,
-				name:	name,
-				incoming:	incoming,
-				outcoming:	outcoming,
-				firstLogin:	firstLogin,
-				onlineTime:	onlineTime,
-				lastTime:	lastTime,	
-				incomingdelta:	incomingdelta,
-				outcomingdelta:	outcomingdelta,
-				channelPath:	channelPath,
-			};
-			var index = 0;
-			for(; index < clients.length; index++){
-				if(clients[index].mac == mac){
-					clients[index] = item;
-					break;
-				}
-			}
-			if(index == clients.length)
-				clients.append(item);
-			device.clients = clients;
-			await ClientModel.findOneAndUpdate({gwId}, {$set: device});	
-			return 1;
-		}catch(err){
-			console.log(err);
-			return 0;
-		}
-
 	}
 
 	generateAuthTokenUrl(gwAddress, gwPort, token, type = '') {
