70e55cc0107eaf1aae3187bd1645d54549071ae0
Dengfeng Liu <liudf0716@gmail.com>
2018-07-20 14:03:28 +0800

    Update client.js

--- routes/client.js
+++ routes/client.js
@@ -5,9 +5,11 @@ import Client from '../controller/setting/client'
 import Check from '../middlewares/check'
 const router = express.Router();
 
-router.all('/devlist', Client.devList);
-router.all('/devlist/online', Client.devListOnline);
-route.all('/cplist', Client.CPList);
-route.all('/cplist/online', Client.CPListOnline);
+router.all('/list', Client.list); // list all client of the router
+router.all('/list/online', Client.listOnline); // list all online clients of the router
+router.all('/list/offline', Client.listOffline);
+route.all('/cplist', Client.cpList); // list all online clients of the channelpath
+route.all('/cplist/online', Client.cplistOnline); // list all online clients of the channelpath
+route.all('/cplist/offline', Client.cplistOffline);
 
 export default router
