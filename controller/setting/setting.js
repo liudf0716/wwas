d60f4a07643698e77d1851bd7e10891946d2337c
caizhibang <caizhibang@kunteng.org>
2018-08-03 15:03:11 +0800

    caizhibang:20180803 modify setting

--- controller/setting/setting.js
+++ controller/setting/setting.js
@@ -10,65 +10,55 @@ class Setting {
     /*
      * configure router's login and portal by gwid
      */
-    async gwidSetting(req, res, next){
+    async wfcSetting(req, res, next){
         try {
-            var gwId = req.body.gwId;
+            var channelPath = req.body.channelPath;
             var gwidSetting = {
-                gwId: gwId,
-                weixin: {
-                    appId: req.body.weixin.appId,
-                    shopId: req.body.weixin.shopId,
-                    secretKey: req.body.weixin.secretKey,
-                    ssid:   req.body.weixin.ssid
-                },
+                channelPath: channelPath,
                 wificoin: {
-                    toAddress:  req.body.wificoin.toAddress,
-                    toAmount:   req.body.wificoin.toAmount
+                    toAddress:  req.body.toAddress,
+                    toAmount:   req.body.toAmount
                 },
                 portalUrl:  req.body.portalUrl,
                 duration:   req.body.duration
             };
-            const result = await GatewayIdModel.findOneAndUpdate({'gwId': gwId}, gwidSetting, {new:true});
+            const result = await ChannelPathModel.findOneAndUpdate({'channelPath': channelPath}, gwidSetting, {new:true});
             if(!result){
-                await GatewayIdModel.create(gwidSetting);
+		res.send({ret_code: 1002, ret_msg: 'FAILED', extra:'WFC设置失败'});
             }
         }catch(err){
             console.log(err);
-            res.send({ret_code: 1002, ret_msg: 'FAILED', extra:'用户输入参数无效'});
+            res.send({ret_code: 1002, ret_msg: 'FAILED', extra:'WFC设置失败'});
         }
-        res.send({ret_code: 0, ret_msg: 'SUCCESS', extra: []});
+        res.send({ret_code: 0, ret_msg: 'SUCCESS', extra: 'WFC设置更新成功'});
     }
     
      /*
      * configure router's login and portal by channelPath
      */
-    async channelpathSetting(req, res, next){
+    async weiXinSetting(req, res, next){
         try {
             var channelPath = req.body.channelPath;
             var channelPathConfig = {
                 channelPath: channelPath,
                 weixin: {
-                    appId: req.body.weixin.appId,
-                    shopId: req.body.weixin.shopId,
-                    secretKey: req.body.weixin.secretKey,
-                    ssid:   req.body.weixin.ssid
-                },
-                wificoin: {
-                    toAddress:  req.body.wificoin.toAddress,
-                    toAmount:   req.body.wificoin.toAmount
+                    appId: req.body.appId,
+                    shopId: req.body.shopId,
+                    secretKey: req.body.secretKey,
+                    ssid:   req.body.ssid
                 },
                 portalUrl:  req.body.portalUrl,
                 duration:   req.body.duration
             };
             const result = await ChannelPathModel.findOneAndUpdate({'channelPath': channelPath}, channelpathSetting, {new:true});
             if(!result){
-                await ChannelPathModel.create(channelpathSetting);
+		res.send({ret_code: 1002, ret_msg: 'FAILED', extra:'微信设置失败'});
             }
         }catch(err){
             console.log(err);
-            res.send({ret_code: 1002, ret_msg: 'FAILED', extra:'用户输入参数无效'});
+            res.send({ret_code: 1002, ret_msg: 'FAILED', extra:'微信设置失败'});
         }
-        res.send({ret_code: 0, ret_msg: 'SUCCESS', extra: []});
+        res.send({ret_code: 0, ret_msg: 'SUCCESS', extra:'微信设置成功'});
     }
 }
 
