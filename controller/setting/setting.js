c20449fcdc5584efff55d5a7ee19cb6f2e98efad
caizhibang <caizhibang@kunteng.org>
2018-08-08 11:25:06 +0800

    caizhibang:20180808 modify setting

--- controller/setting/setting.js
+++ controller/setting/setting.js
@@ -39,7 +39,7 @@ class Setting {
     async weiXinSetting(req, res, next){
         try {
             var channelPath = req.body.user_account;
-            var channelPathConfig = {
+            var channelPathSetting = {
                 channelPath: channelPath,
                 weixin: {
                     appId: req.body.appId,
@@ -50,7 +50,7 @@ class Setting {
                 portalUrl:  req.body.portalUrl,
                 duration:   req.body.duration
             };
-            const result = await ChannelPathModel.findOneAndUpdate({'channelPath': channelPath}, channelpathSetting, {new:true});
+            const result = await ChannelPathModel.findOneAndUpdate({'channelPath': channelPath}, channelPathSetting, {new:true});
             if(!result){
 		res.send({ret_code: 1002, ret_msg: 'FAILED', extra:'微信设置失败'});
             }
