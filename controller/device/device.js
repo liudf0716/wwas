340067b2df9e897e87fe7221046fab80dfccfba0
caizhibang <caizhibang@kunteng.org>
2018-08-09 16:31:41 +0800

    caizhibang:20180809 modify device

--- controller/device/device.js
+++ controller/device/device.js
@@ -59,7 +59,7 @@ class deviceHandle {
                 //如果采用返回值得形式，必须的await
                 var query = await DeviceModel.findOne(wherestr).exec();
                 if (query != null) {
-                    if (query.channelPath === '') {
+                    if (query.channelPath === '' || query.channelPath === 'wificoin') {
                         var updatestr = { 'channelPath': user_name };
                         await DeviceModel.findByIdAndUpdate(query['_id'], updatestr).exec();
                     } else {
