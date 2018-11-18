be81f4c736974e8dc9aca3d3576f7a793383c084
Dengfeng Liu <liudf0716@qq.com>
2018-11-18 20:18:21 +0800

    Update wifidog.js

--- routes/wifidog.js
+++ routes/wifidog.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/ping', WiFiDog.checkPingParam, WiFiDog.ping);
 router.get('/offline', WiFiDog.checkPingParam, WiFiDog.offline);
-router.get('/auth', WiFiDog.checkAuthParam, WiFiDog.auth);
+router.all('/auth', WiFiDog.checkAuthParam, WiFiDog.auth);
 router.get('/login', WiFiDog.checkLoginParam,  WiFiDog.login);
 router.get('/portal', WiFiDog.checkPortalParam,  WiFiDog.portal);
 router.get('/auth/wfc', WiFiDog.checkAuthWfcParam, WiFiDog.authWfc);
