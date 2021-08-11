37ea5c195bd58a72631e33c5c2665e97b796a380
Dengfeng Liu <liudf0716@qq.com>
2021-08-11 19:54:28 +0800

    Update development.js

--- config/development.js
+++ config/development.js
@@ -5,7 +5,7 @@ module.exports = {
 	mongoUrl: 'mongodb://127.0.0.1:27017/wfc',
 	insightApi: 'https://insight.wificoin.club/insight-api',
 	wfcPayUrl: 'https://wfc.wificoin.club/#/wifiPortal/payment?authServer=',
-	authDomain: 'http://localhost',
+	authDomain: 'http://172.96.252.145:8080',
 	wfcAuthPath: '/wifidog/auth/wfc',
 	wxAuthPath: '/wifidog/auth/weixin',
 	device_dir: './public/device',
